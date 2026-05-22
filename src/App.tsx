8bffa1f7d9c3c360cba1641a59eb44410f37752d
maira333-mmm <maira.alam33@gmail.com>
2026-05-22 05:43:09 -0700

    remove node_modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
 import CarDetail from './pages/CarDetail';
-
 
 function App() {
   return (
@@ -16,11 +13,8 @@ function App() {
         <Route path="/services" element={<Services />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
         <Route path="/car/:id" element={<CarDetail />} />
       </Routes>
-   
     </Router>
   );
 }
